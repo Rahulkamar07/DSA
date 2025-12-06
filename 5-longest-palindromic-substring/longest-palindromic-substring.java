9da82c40631791975b978f7b5e313173d9916ff8
Rahul Kumar Amar <92214008+Rahulkamar07@users.noreply.github.com>
2025-12-06 22:52:40 +0530

    Time: 14 ms (90.72%) | Memory: 43.4 MB (63.73%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0)
+            return "";
+
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int len1 = expand(s, i, i);     // odd-length center
+            int len2 = expand(s, i, i + 1); // even-length center
+            int len = Math.max(len1, len2);
+
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        // return result using StringBuilder
+        return new StringBuilder(s.substring(start, end + 1)).toString();
+    }
+
+    private int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
